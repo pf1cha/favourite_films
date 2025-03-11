e12455e79cd330bc2e6a5400da03c7214c2aa9d6
Max <lonishin.max@gmail.com>
2025-03-11 14:15:24 +0300

    realisation part which works with api

---
+++ main.py
@@ -0,0 +1,14 @@
+from backend.api.api_utils import get_movie_info
+
+# movie_data = get_movie_info('Inception')
+#
+# if movie_data:
+#     print("Title:", movie_data.get('Title'))
+#     print("Year:", movie_data.get('Year'))
+#     print("Runtime:", movie_data.get('Runtime'))
+#     print("Genre:", movie_data.get('Genre'))
+#     print("Transformed Ratings:", movie_data.get('Ratings'))
+#     print("Plot:", movie_data.get('Plot'))
+#     print("Director", movie_data.get("Director"))
+# else:
+#     print("Could not retrieve movie information.")
