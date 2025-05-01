fd824aad81d7720a35d19e4e375cf82f73e7d604
Max <lonishin.max@gmail.com>
2025-05-01 22:47:44 +0300

    make some changes into config.py

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,7 @@
-# backend/config.py
 import os
 from dotenv import load_dotenv
+import sys
 
-# Загружаем переменные из .env файла
 load_dotenv()
 
 OMDB_API_KEY = os.getenv("OMDB_API_KEY")
@@ -12,11 +11,12 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 
-# Проверка, что основные переменные загружены
 if not OMDB_API_KEY:
     print("Ошибка: Ключ OMDb API не найден в .env")
+    sys.exit(1)
 if not DB_NAME or not DB_USER or not DB_PASSWORD:
     print("Ошибка: Данные для подключения к БД не найдены в .env")
+    sys.exit(1)
 
 DATABASE_CONFIG = {
     'dbname': DB_NAME,
