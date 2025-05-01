84d077235b02c979948e53bb4c18ea47cd999cd7
Max <lonishin.max@gmail.com>
2025-05-01 22:37:49 +0300

    add a template for login system

---
+++ frontend/login_window.py
@@ -0,0 +1,10 @@
+# login_window.py
+from PyQt6.QtWidgets import (
+    QDialog, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit,
+    QPushButton, QMessageBox, QFormLayout
+)
+from PyQt6.QtCore import pyqtSignal, Qt
+from backend import auth
+
+class LoginWindow(QDialog):
+    pass
