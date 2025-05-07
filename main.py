be216f05a2ee69ab96aea86d447e461e15e91826
Max <lonishin.max@gmail.com>
2025-05-08 01:06:33 +0300

    resolve conflict

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# main.py
 import sys
+import os
 from PyQt6.QtWidgets import QApplication, QDialog
 from frontend.login_window import LoginWindow
-from frontend.movie_app_window import MovieAppWindow  # Changed import
+from frontend.movie_app_window import MovieApp
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
     login_dialog = LoginWindow()
     main_window = None
 
@@ -13,13 +14,14 @@ if __name__ == '__main__':
     def on_login_success(user_id: int):
         global main_window
         print(f"Вход успешен (User ID: {user_id}). Запуск основного приложения...")
-        main_window = MovieAppWindow(user_id=user_id)
+        main_window = MovieApp(user_id=user_id)
         main_window.show()
 
 
-    login_dialog.login_successful.connect(on_login_success)
-    dialog_result = login_dialog.exec()
-    if dialog_result == QDialog.DialogCode.Accepted and main_window:
+    # login_dialog.login_successful.connect(on_login_success)
+    result = login_dialog.exec()
+
+    if result == QDialog.DialogCode.Accepted and main_window:
         print("Запуск главного цикла событий приложения...")
         sys.exit(app.exec())
     else:
